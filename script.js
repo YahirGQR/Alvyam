d8e5aed01045fb50f5f25b27822b3724c78beb83
YahirGQR <111616272+YahirGQR@users.noreply.github.com>
2025-05-06 20:13:28 -0600

    \Mejora en los archivos del proyecto"

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Página ALVYAM cargada correctamente");
+
+// Modo oscuro
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.createElement("button");
+  btn.id = "darkModeToggle";
+  btn.innerHTML = "🌙";
+  document.body.appendChild(btn);
+
+  btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    btn.innerHTML = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+  });
+});
